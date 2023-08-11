bc9ea1ae288fc4643f36e3b929420c79eaa7515f
Fabiana Cervini <fabiana.cervini@outlook.com>
2023-08-11 18:54:01 -0300

    sumando boton

--- app/app.js
+++ app/app.js
@@ -1,19 +1,32 @@
 const contadorElemento = document.getElementById("contador");
-const incrementarBoton = document.getElementById("incrementar");
+const sumarBoton = document.getElementById("sumar");
+const restarBoton = document.getElementById("restar");
 const borrarBoton = document.getElementById("borrar");
 
 // Obtener el contador actual del almacenamiento local
 let contador = parseInt(localStorage.getItem("contadorBesos")) || 0;
 contadorElemento.textContent = contador;
 
-// Actualizar el contador y almacenarlo en el almacenamiento local
-incrementarBoton.addEventListener("click", () => {
-    contador++;
+// Función para actualizar el contador y almacenarlo en el almacenamiento local
+function actualizarContador(valor) {
+    contador += valor;
     contadorElemento.textContent = contador;
     localStorage.setItem("contadorBesos", contador.toString());
+}
+
+// Botón para sumar un beso
+sumarBoton.addEventListener("click", () => {
+    actualizarContador(1);
 });
 
-// Borrar el contador y actualizar la página
+// Botón para restar un beso
+restarBoton.addEventListener("click", () => {
+    if (contador > 0) {
+        actualizarContador(-1);
+    }
+});
+
+// Botón para borrar el contador
 borrarBoton.addEventListener("click", () => {
     contador = 0;
     contadorElemento.textContent = contador;
