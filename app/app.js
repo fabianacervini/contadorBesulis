d99b6ca91ad797b6fdb9fa6d6c247da35967dc63
Fabiana Cervini <fabiana.cervini@outlook.com>
2023-08-11 19:51:08 -0300

    modificando js

--- app/app.js
+++ app/app.js
@@ -1,34 +1,48 @@
-const contadorElemento = document.getElementById("contador");
+const deudaElemento = document.getElementById("deuda");
 const sumarBoton = document.getElementById("sumar");
 const restarBoton = document.getElementById("restar");
-const borrarBoton = document.getElementById("borrar");
+const borrarDeudaBoton = document.getElementById("borrarDeuda");
+const cantidadInput = document.getElementById("cantidad");
 
-// Obtener el contador actual del almacenamiento local
-let contador = parseInt(localStorage.getItem("contadorBesos")) || 0;
-contadorElemento.textContent = contador;
+// Obtener la deuda actual del almacenamiento local
+let deuda = parseInt(localStorage.getItem("deudaBesos")) || 0;
+deudaElemento.textContent = deuda;
 
-// Función para actualizar el contador y almacenarlo en el almacenamiento local
-function actualizarContador(valor) {
-    contador += valor;
-    contadorElemento.textContent = contador;
-    localStorage.setItem("contadorBesos", contador.toString());
+// Función para actualizar la deuda y almacenarla en el almacenamiento local
+function actualizarDeuda(valor) {
+    deuda += valor;
+    deudaElemento.textContent = deuda;
+    localStorage.setItem("deudaBesos", deuda.toString());
 }
 
-// Botón para sumar un beso
+// Evento para procesar la acción de sumar 1 a la deuda
 sumarBoton.addEventListener("click", () => {
-    actualizarContador(1);
+    actualizarDeuda(1);
+    cantidadInput.value = "";
 });
 
-// Botón para restar un beso
+// Evento para procesar la acción de restar 1 de la deuda
 restarBoton.addEventListener("click", () => {
-    if (contador > 0) {
-        actualizarContador(-1);
+    if (deuda > 0) {
+        actualizarDeuda(-1);
+    }
+    cantidadInput.value = "";
+});
+
+// Evento para sumar la deuda al presionar "Enter"
+cantidadInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        const cantidad = parseInt(cantidadInput.value) || 0;
+        if (cantidad > 0) {
+            actualizarDeuda(cantidad);
+            cantidadInput.value = "";
+        }
     }
 });
 
-// Botón para borrar el contador
-borrarBoton.addEventListener("click", () => {
-    contador = 0;
-    contadorElemento.textContent = contador;
-    localStorage.removeItem("contadorBesos");
+// Botón para borrar la deuda
+borrarDeudaBoton.addEventListener("click", () => {
+    deuda = 0;
+    deudaElemento.textContent = deuda;
+    localStorage.removeItem("deudaBesos");
 });
